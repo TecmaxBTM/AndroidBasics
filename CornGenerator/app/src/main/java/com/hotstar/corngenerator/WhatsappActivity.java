d0419e287f549ccae68334e6dddd49d1a9789a00
Mahesh <mahesh@glowroad.com>
2019-05-01 11:31:01 +0530

    Alert Dialog With Fragment BackStack

--- CornGenerator/app/src/main/java/com/hotstar/corngenerator/WhatsappActivity.java
+++ CornGenerator/app/src/main/java/com/hotstar/corngenerator/WhatsappActivity.java
@@ -1,5 +1,6 @@
 package com.hotstar.corngenerator;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -10,9 +11,11 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -33,6 +36,7 @@ public class WhatsappActivity extends AppCompatActivity implements SignUpListene
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                showMyDi();
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
@@ -87,10 +91,34 @@ public class WhatsappActivity extends AppCompatActivity implements SignUpListene
         }
     }
 
-    public static void replaceFrag(Fragment fg) {
+    public static void replaceFrag(Fragment fg, boolean isStack) {
         FragmentTransaction sd = fm.beginTransaction();
         sd.replace(R.id.containerLV, fg, fg.getClass().getSimpleName());
+        if (isStack)
+            sd.addToBackStack(fg.getClass().getSimpleName());
         sd.commit();
     }
+
+    void showMyDi() {
+        AlertDialog.Builder zxz = new AlertDialog.Builder(this);
+        zxz.setTitle("FeedBack On Android App");
+        zxz.setMessage("Valuable Feedback on how classes going");
+        zxz.setPositiveButton("Like", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(WhatsappActivity.this, "Thanks For Rating", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        zxz.setNegativeButton("DisLike", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(WhatsappActivity.this, "We will look into issues", Toast.LENGTH_SHORT).show();
+            }
+        });
+        AlertDialog abs = zxz.create();
+        abs.show();
+    }
+
 
 }
---
+++ CornGenerator/app/src/main/java/com/hotstar/corngenerator/recyclerview/RecycleActivity.java
@@ -0,0 +1,15 @@
+package com.hotstar.corngenerator.recyclerview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hotstar.corngenerator.R;
+
+public class RecycleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycle);
+    }
+}
